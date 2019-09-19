845367d6c223e48901d7089757027d34b704f7d3
Mao <amcaicedo@unimayor.edu.co>
2019-09-19 22:41:52 +0000

    Exercise 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int number = 48;
+
+	for (; number < 58 ; number++)
+	{
+		putchar(number);
+		if (number < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
