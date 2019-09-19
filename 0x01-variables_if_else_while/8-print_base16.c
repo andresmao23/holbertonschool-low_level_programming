2e48ab5188495e856a538714538d9c700c4e85fb
Mao <amcaicedo@unimayor.edu.co>
2019-09-19 22:30:07 +0000

    Hexadecimal, exercise 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int number = 48;
+	char letra = 'a';
+
+	for (; number < 58 ; number++)
+		putchar(number);
+
+	for (; letra <= 'f' ; letra++)
+		putchar(letra);
+
+	putchar('\n');
+	return (0);
+
+}
