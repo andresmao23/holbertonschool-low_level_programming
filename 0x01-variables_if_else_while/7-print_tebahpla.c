f2dd9e0016ef62fb816df196a3dbea3c63e22858
Mao <amcaicedo@unimayor.edu.co>
2019-09-19 21:44:49 +0000

    Smile in the mirror, exercise 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int a = 122;
+
+	for (; a > 96 ; a--)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+
+}
