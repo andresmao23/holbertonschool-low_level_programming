76a196d547b5e4204e7b9deb561bf4aeecb40b7c
Mao <amcaicedo@unimayor.edu.co>
2019-09-19 20:53:17 +0000

    Alphabet except e and q, exercise 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int a = 'a';
+	int z = 'z';
+
+	for (; a <= z ; a++)
+	{
+		if ( a == 101 || a == 113  )
+			a++;
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,5 @@
 * Positive anything is better than negative nothing, exercise 0
 * Last digit, exercise 1
 * I sometimes suffer from insomnia. And when I cant fall asleep, i play what i call the alphabet game
+* alphaBET, exercise 3
+* print alphabt, exercise 4
