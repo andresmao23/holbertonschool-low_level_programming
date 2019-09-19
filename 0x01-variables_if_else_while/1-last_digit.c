85784e6e4bd5168d6c35f9e3c9b47a96c2443a4c
Mao <amcaicedo@unimayor.edu.co>
2019-09-19 19:13:53 +0000

    The last digit, exercise 1, fix 4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("Last digit of %i is %i and is 0\n", n, last);
-	else (last < 6)
+	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
