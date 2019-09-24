d946d7d5fabeb70866ec2adbaaf972c513d5ab90
Mao <amcaicedo@unimayor.edu.co>
2019-09-24 15:37:50 +0000

    The last digit, exercise 7, fix 4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,18 +9,16 @@
 int print_last_digit(int n)
 {
 
-	int last;
+	int last = n % 10;
 
-	if (n <= 0)
+	if (last <= 0)
 	{
-		last = (n * -1) % 10;
+		last = -last;
 		_putchar(last + '0');
 	}
 	else
-	{
-		last = n % 10;
 		_putchar(last + '0');
-	}
+
 	return (last);
 
 }
