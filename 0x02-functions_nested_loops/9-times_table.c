c4d8ea75bf1678e904b3062fc09b4f4331a26da1
Mao <amcaicedo@unimayor.edu.co>
2019-09-24 19:13:30 +0000

    Times table, exercise 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * times_table - prints a table
+ *
+ */
+void times_table(void)
+{
+	int n, i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			if (n != 9)
+			{
+				if ((i * n) > 9)
+				{
+					_putchar(((i * n) / 10) + '0');
+					_putchar(((i * n) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(i * n + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				if ((i * n) > 9)
+				{
+					_putchar(((i * n) / 10) + '0');
+					_putchar(((i * n) % 10) + '0');
+				}
+				else
+					_putchar(i * n + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
