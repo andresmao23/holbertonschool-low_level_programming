4671c5bc58873b07c3414b7a06fd853805ec097e
Mao <amcaicedo@unimayor.edu.co>
2019-09-24 16:35:52 +0000

    Jack bauer, exercise 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints the jack bauers time
+ *
+ */
+void jack_bauer(void)
+{
+
+	int n;
+	int m;
+
+	for (n = 0; n <= 24; n++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			if ((n == 0 && m != 0) || (m > n))
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				_putchar(':');
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+				_putchar('\n');
+			}
+		}
+	}
+
+}
