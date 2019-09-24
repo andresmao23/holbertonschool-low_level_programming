05d31ecdc39971bd84bb31846f35bcb98c116bea
Mao <amcaicedo@unimayor.edu.co>
2019-09-24 13:35:40 +0000

    Is alpha, exercise 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - check if the parameter is alpha
+ *
+ * @c: parameter to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+
+}
