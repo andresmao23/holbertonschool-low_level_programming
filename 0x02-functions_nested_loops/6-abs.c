fdbd64bb5dce169c56b92684b7962afe0076cea2
Mao <amcaicedo@unimayor.edu.co>
2019-09-24 14:15:04 +0000

    Absolute value, ecercise 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @n: parameter to check
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
