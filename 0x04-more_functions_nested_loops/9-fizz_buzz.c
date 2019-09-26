bc4c146190caa06ccba9a7a9cbb7c389032a20c6
Mao <amcaicedo@unimayor.edu.co>
2019-09-26 20:28:41 +0000

    Fizz Buzz, exercise 9, fix 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,24 @@ int main(void)
 
 	for (; i <= 100 ; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+		if (i != 100)
+		{
+			if (i % 3 == 0)
+				printf("Fizz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%i ", i);
+		}
 		else
-			printf("%i ", i);
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			else if (i % 5 == 0)
+				printf("Buzz");
+			else
+				printf("%i", i);
+		}
 	}
 	printf("\n");
 	return (0);
