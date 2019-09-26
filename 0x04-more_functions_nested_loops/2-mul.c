fd6da8411edf8a2c2de969577b8a65d247053c42
Mao <amcaicedo@unimayor.edu.co>
2019-09-26 15:12:19 +0000

    Mul, exercise 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply two numbers.
+ *
+ * @a: parameter to multiply
+ * @b: parameter to multiply
+ *
+ * Return: multiply result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 # 0x04-more_functions_nested_loops
 * isupper, exercise 0
 * is digit, exercise 1
+* mul, exercise 2
