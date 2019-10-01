260b9db0445ba25ca552c70714c5140ffb610ee1
Mao <amcaicedo@unimayor.edu.co>
2019-10-01 21:21:02 +0000

    Exercise 5, fix 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,20 +9,17 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int j = 0;
-	int len = 0;
-	char temp[10];
+	int i, temp, j = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	i--;
+	while (j < i)
 	{
-		temp[i] = s[i];
-		len++;
-	}
-
-	for (i = len - 1 ; i >= 0 ; i--)
-	{
-		s[j] = temp[i];
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
 		j++;
+		i--;
 	}
 }
