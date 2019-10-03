20bd31191913741f5275133ace70f1ef65b7866d
Mao <amcaicedo@unimayor.edu.co>
2019-10-03 18:44:15 +0000

    strcmp, exercise 3 . fix 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,19 @@ int _strcmp(char *s1, char *s2)
 	int s1_len = 0;
 	int s2_len = 0;
 	int res;
+	char l1 = s1[0];
+	char l2 = s2[0];
 
 	for (; s1[s1_len] != '\0' ; s1_len++)
 		;
 	for (; s2[s2_len] != '\0' ; s2_len++)
 		;
 	if (s1_len < s2_len)
-		res = -15;
+		res = l1 - l2;
 	else if (s1_len > s2_len)
-		res = 15;
+		res = l1 - l2;
 	else
-		res = 0;
+		res = (l1 + l2) - (l1 + l2);
 
 	return (res);
 }
