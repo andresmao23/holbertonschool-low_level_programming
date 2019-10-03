6bea8e4e4c62dd7c625f3af52b2443f055c5bbe3
Mao <amcaicedo@unimayor.edu.co>
2019-10-03 15:54:08 +0000

    strncat, exercise 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncat - copy a string to the end of another string.
+ *
+ * @dest: char array pointer destination
+ * @src: char array source
+ * @n: zise
+ *
+ * Return: @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	while(dest[dest_len] != '\0')
+		dest_len++;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
