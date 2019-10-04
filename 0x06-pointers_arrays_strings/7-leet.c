a053849f78e9dd87261f36671e9ca9d0116053b6
Mao <amcaicedo@unimayor.edu.co>
2019-10-04 00:23:25 +0000

    leet, exercise 7 - incomplete

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * leet - encode a phrase.
+ *
+ * @s: string to check
+ *
+ * Return: @s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char may[] = {'A', 'E', 'O', 'T', 'L'};
+	char min[] = {'a', 'e', 'o', 't', 'l'};
+	char num[] = {52, 51, 48, 55, 49};
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 5 ; j++)
+		{
+			
+		}
+	}
+	return (s);
+}
